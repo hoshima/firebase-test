e2f9c4ae78bcc996e723a7a0c30f49bb9328494c
hoshima <hoshima@users.noreply.github.com>
2024-10-13 17:21:10 +0900

    firebase情報をenvironmentファイルに切り出し

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,22 +8,13 @@ import {
   provideAnalytics,
   ScreenTrackingService,
 } from '@angular/fire/analytics';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideFirebaseApp(() =>
-      initializeApp({
-        projectId: 'fir-test-bac4d',
-        appId: '1:760236729898:web:faa0ef7d5b2f587eaa0553',
-        storageBucket: 'fir-test-bac4d.appspot.com',
-        apiKey: 'AIzaSyAVLJLYRZkOhvGsz_ETRuuMyxklCGJqrYc',
-        authDomain: 'fir-test-bac4d.firebaseapp.com',
-        messagingSenderId: '760236729898',
-        measurementId: 'G-J3CHELPC7Z',
-      })
-    ),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAnalytics(() => getAnalytics()),
     ScreenTrackingService,
   ],
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'fir-test-bac4d',
+    appId: '1:760236729898:web:faa0ef7d5b2f587eaa0553',
+    storageBucket: 'fir-test-bac4d.appspot.com',
+    apiKey: 'AIzaSyAVLJLYRZkOhvGsz_ETRuuMyxklCGJqrYc',
+    authDomain: 'fir-test-bac4d.firebaseapp.com',
+    messagingSenderId: '760236729898',
+    measurementId: 'G-J3CHELPC7Z',
+  },
+};
